e3a639aba79301dcb0b17309ed3f03ce9ec44d50
Gaurav Patil <gauravpatil6624@gmail.com>
2022-11-23 12:41:00 +0530

    Add uc1 File

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Day_3_Practice_EmployeeWage
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Not Present");
+            }
+
         }
     }
 }
-
