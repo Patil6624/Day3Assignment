014a93a8c4e212649c0364447ef2a1a0f29110fe
Gaurav Patil <gauravpatil6624@gmail.com>
2022-11-23 12:27:50 +0530

    Add Welcome Message

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Day_4_Practice_EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation");
+        }
+    }
+}
 
