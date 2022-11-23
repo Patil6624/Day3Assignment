fe3e7972ed97e394e4ec3a76785b7c71ae0b76c2
Gaurav Patil <gauravpatil6624@gmail.com>
2022-11-23 12:54:43 +0530

    Add uc4 File

--- Program.cs
+++ Program.cs
@@ -3,26 +3,33 @@ namespace Day_3_Practice_EmployeeWage
 {
     class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation");
-            int IS_FULL_TIME = 1;
-            int EMP_RATE_PER_HOUR = 20;
+
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
 
-            if (empCheck == IS_FULL_TIME)
+            switch (empWage)
             {
-                Console.WriteLine("Employee is Present");
-                empHrs = 8;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is Not Present");
-                empHrs = 0;
-            }
+
 
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("EMP Wage : " + empWage);
